864de4a042110f186bdbd9f65b61a9a70d7e58cc
Youssef Chennou <Youssef.Chennou@brussels.msf.org>
2021-08-25 16:43:14 +0200

    ajout package Unitaire

---
+++ src/be/icc/poo/testUnitaire/TestConcert.java
@@ -0,0 +1,14 @@
+package be.icc.poo.testUnitaire;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestConcert {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
